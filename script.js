5b1c32ba25fe4430bf12cde2b69e102f0ed7cf2d
e282272 <e282272@sbl.ch>
2023-12-07 15:43:02 +0100

    end of lesson 07.12.

--- script.js
+++ script.js
@@ -1,10 +1,32 @@
-export function exercise01(args) {
+export function exercise01(args) /*defining the function*/ {
   const input = args
   const result = []
 
   for (let i = 0; i < input.length; i++) {
     const currentElement = input[i]
-    result.push(currentElement)
+    if (currentElement === "e" || currentElement === "E") {
+      result.push("")
+    } else {
+      result.push(currentElement)
+    }
   }
   return result.join("")
+}
+
+export function exercise02(args) {
+  return args.toUpperCase()
+}
+export function exercise03(args) {
+  const input = args
+  const result = []
+  let count = 0
+
+  for (let i = 0; i < input.length; i++) {
+    const currentElement = input[i]
+    if (currentElement === "e") count = count + 1
+    else if (currentElement === "E") {
+      count = count + 1
+    }
+  }
+  return count
 }
